5b0d1d0343402b9f4252acf624772cb78e0192fe
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-06-27 20:26:53 -0300

    Initializing project

---
+++ Trade iOS/Trade iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Trade iOS
+//
+//  Created by Jhonathan Wyterlin on 27/06/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Trade iOS/Trade iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Trade iOS
+//
+//  Created by Jhonathan Wyterlin on 27/06/15.
+//  Copyright © 2015 Jhonathan Wyterlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
